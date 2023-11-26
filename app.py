bebc013a8c1b97a9465680001527d7490ae70539
Deep <patelbhai2981@gmail.com>
2023-11-25 21:46:06 -0500

    updates

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def save_reminders(file_name, reminders):
     with open(file_name, 'w') as file:
         json.dump(reminders, file)
 
+
 # Variables to hold the current reminders
 current_general_reminders = []
 current_medications = []
@@ -36,60 +37,79 @@ current_appointments.clear()
 current_appointments.extend(load_reminders('data/appointments.json'))
 
 # Dictionary to hold the current reminders
-current_reminders = {'general': None, 'medications': None, 'appointments': None}
+current_reminders = {'general': None,
+                     'medications': None, 'appointments': None}
 
-# Function to schedule notification for reminders
-def schedule_notification(reminder_text, reminder_time):
+def add_reminder_with_date(reminder_type, reminder_text, reminder_date, reminder_time):
+    global current_reminders
+
+    reminder_datetime = datetime.datetime.strptime(
+        f"{reminder_date} {reminder_time}", "%Y-%m-%d %H:%M")
+    current_time = datetime.datetime.now()
+
+    if reminder_datetime > current_time:
+        reminder = (reminder_text,
+                    reminder_datetime.strftime('%Y-%m-%d %H:%M'))
+
+        if reminder_type == 'general':
+            current_general_reminders.append(reminder)
+            save_reminders('data/general_reminders.json',
+                           current_general_reminders)
+        elif reminder_type == 'appointments':
+            current_appointments.append(reminder)
+            save_reminders('data/appointments.json', current_appointments)
+
+        # Schedule reminders for the specified date and time
+        thread = threading.Thread(target=schedule_notification, args=(
+            reminder_text, reminder_date, reminder_time))
+        thread.daemon = True
+        thread.start()
+
+def schedule_notification(reminder_text, reminder_date, reminder_time):
     while True:
-        current_time = datetime.datetime.now().strftime('%H:%M')  # Get current time in 24-hour format
-        if current_time == reminder_time:
+        current_datetime = datetime.datetime.now()
+        current_date = current_datetime.strftime(
+            '%Y-%m-%d')  # Get current date
+        current_time = current_datetime.strftime(
+            '%H:%M')     # Get current time
+
+        # Check if current date and time match the reminder date and time
+        if current_date == reminder_date and current_time == reminder_time:
             notification_title = 'Reminder'
             notification_text = f"Don't forget: {reminder_text}"
-            subprocess.run(['osascript', '-e', f'display notification "{notification_text}" with title "{notification_title}"'])
+            subprocess.run(
+                ['osascript', '-e', f'display notification "{notification_text}" with title "{notification_title}"'])
             break
+
         # Check every second
         time.sleep(1)
 
-# Function to add a reminder
-def add_reminder(reminder_type, reminder_text, reminder_time):
-    global current_reminders
-    reminder = (reminder_text, reminder_time)
-    if reminder_type == 'general':
-        current_reminders['general'] = reminder
-        current_general_reminders.append(reminder)
-        save_reminders('data/general_reminders.json', current_general_reminders)
-    elif reminder_type == 'medications':
-        current_reminders['medications'] = reminder
-        current_medications.append(reminder)
-        save_reminders('data/medications.json', current_medications)
-    elif reminder_type == 'appointments':
-        current_reminders['appointments'] = reminder
-        current_appointments.append(reminder)
-        save_reminders('data/appointments.json', current_appointments)
-
-    # Start a thread for the new reminder
-    thread = threading.Thread(target=schedule_notification, args=(reminder_text, reminder_time))
-    thread.daemon = True
-    thread.start()
-
+# Updated route to handle adding reminders with date and time
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global current_reminders, current_general_reminders, current_medications, current_appointments
+    global current_general_reminders, current_medications, current_appointments
+
     if request.method == 'POST':
         reminder_type = request.form['reminder_type']
         reminder_text = request.form['reminder']
+        reminder_date = request.form['date']  # Get the date from the form
         reminder_time = request.form['time']
-        add_reminder(reminder_type, reminder_text, reminder_time)
-    return render_template('index.html', current_general=current_general_reminders, current_medications=current_medications, current_appointments=current_appointments, current_reminders=current_reminders)
+
+        add_reminder_with_date(reminder_type, reminder_text,
+                               reminder_date, reminder_time)
+
+    return render_template('index.html', current_general=current_general_reminders, current_medications=current_medications, current_appointments=current_appointments)
+
 
 @app.route('/medication', methods=['GET', 'POST'])
 def medication_reminder():
     global current_medications
-    
+
     if request.method == 'POST':
         reminder_type = request.form['reminder_type']
         medication_name = request.form['medication_name']
         medication_dose = request.form['medication_dose']
+        reminder_date = request.form['date']  # Get the date from the form
         # Get a list of reminder times selected by the user
         reminder_times = request.form.getlist('reminder_times')
 
@@ -97,6 +117,7 @@ def medication_reminder():
         medication_details = {
             'medication_name': medication_name,
             'medication_dose': medication_dose,
+            'reminder_date': reminder_date,  # Include the reminder date
             'reminder_times': reminder_times
         }
 
@@ -106,66 +127,107 @@ def medication_reminder():
 
         # Schedule reminders for each selected time
         for time in reminder_times:
-            add_reminder(reminder_type, f"Take {medication_name} ({medication_dose})", time)
+            add_reminder_with_date(
+                reminder_type, f"Take {medication_name} ({medication_dose})", reminder_date, time)
 
-    return render_template('medication.html', current_medications=current_medications)
+    return render_template('index.html', current_medications=current_medications)
+
 
 @app.route('/appointment', methods=['POST'])
 def add_appointment_reminder():
+    global current_appointments
+
     if request.method == 'POST':
         reminder_type = request.form['reminder_type']
         reminder_text = request.form['reminder']
+        reminder_date = request.form['date']  # Get the date from the form
         reminder_time = request.form['time']
-        add_reminder(reminder_type, reminder_text, reminder_time)
-    return render_template('index.html', current_general=current_general_reminders, current_medications=current_medications, current_appointments=current_appointments, current_reminders=current_reminders)
+
+        add_reminder_with_date(reminder_type, reminder_text,
+                               reminder_date, reminder_time)
+
+    return render_template('index.html', current_general=current_general_reminders, current_medications=current_medications, current_appointments=current_appointments)
 
 
 @app.route('/delete_reminder', methods=['POST'])
 def delete_reminder():
     reminder_type = request.json['reminder_type']
-    index = int(request.json['index'])  # Convert index to integer
-    
+    index = int(request.json['index'])
+
     if reminder_type == 'general':
         global current_general_reminders
+        current_general_reminders = load_reminders(
+            'data/general_reminders.json')
         if 0 <= index < len(current_general_reminders):
-            del current_general_reminders[index]  # Delete the specific reminder
-            save_reminders('data/general_reminders.json', current_general_reminders)
-            current_reminders['general'] = None if not current_general_reminders else current_general_reminders[0]
+            del current_general_reminders[index]
+            save_reminders('data/general_reminders.json',
+                           current_general_reminders)
     elif reminder_type == 'medications':
         global current_medications
+        current_medications = load_reminders('data/medications.json')
         if 0 <= index < len(current_medications):
-            del current_medications[index]  # Delete the specific reminder
+            del current_medications[index]
             save_reminders('data/medications.json', current_medications)
-            current_reminders['medications'] = None if not current_medications else current_medications[0]
     elif reminder_type == 'appointments':
         global current_appointments
+        current_appointments = load_reminders('data/appointments.json')
         if 0 <= index < len(current_appointments):
-            del current_appointments[index]  # Delete the specific reminder
+            del current_appointments[index]
             save_reminders('data/appointments.json', current_appointments)
-            current_reminders['appointments'] = None if not current_appointments else current_appointments[0]
+
     return jsonify({'success': True})
 
+# --------------------------Health Page--------------------------
 
-# Variable to hold health metrics
-health_metrics = []
+# Function to load health metrics from JSON file
+def load_health_metrics(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
 
-# Function to add health metrics
-def add_health_metrics(blood_pressure, heart_rate, other_metric):
+# Function to save health metrics to JSON file
+def save_health_metrics(file_name, health_metrics):
+    with open(file_name, 'w') as file:
+        json.dump(health_metrics, file)
+
+# Load health metrics once when the app starts
+health_metrics = load_health_metrics('data/health_data.json')
+
+# Function to handle health metrics
+def add_health_metrics(blood_pressure, heart_rate, other_metric, health_date):
     global health_metrics
     metrics = {
         'blood_pressure': blood_pressure,
         'heart_rate': heart_rate,
-        'other_metric': other_metric  # Add more metrics as needed
+        'other_metric': other_metric,
+        'health_date': health_date  # Include the health date
     }
     health_metrics.append(metrics)
+    save_health_metrics('data/health_data.json', health_metrics)
 
+# Flask route for health metrics with search functionality
 @app.route('/health', methods=['GET', 'POST'])
 def health_tracker():
+    global health_metrics
+
     if request.method == 'POST':
         blood_pressure = request.form['blood_pressure']
         heart_rate = request.form['heart_rate']
         other_metric = request.form['other_metric']
-        add_health_metrics(blood_pressure, heart_rate, other_metric)
+        health_date = request.form['date']  # Retrieve the health date from the form
+        
+        # Add health metrics along with the date
+        add_health_metrics(blood_pressure, heart_rate, other_metric, health_date)
+        # Reload the health metrics after adding new data
+        health_metrics = load_health_metrics('data/health_data.json')
+
+    # Search health metrics by date if a date parameter is present in the request
+    search_date = request.args.get('date')
+    if search_date:
+        filtered_metrics = [metric for metric in health_metrics if metric['health_date'] == search_date]
+        return jsonify(filtered_metrics)
     
     return render_template('health.html', health_metrics=health_metrics)
 
